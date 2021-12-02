3a849ef51d1f313c528ee24a43df80f75463d33a
Tilo Neumann <tilo.neumann@gbv.de>
2021-12-02 12:05:50 +0100

    FHSWF-20 updates start page, adds text and search field

--- resources/xsl/mir-flatmir-layout-utils.xsl
+++ resources/xsl/mir-flatmir-layout-utils.xsl
@@ -197,12 +197,6 @@
   <xsl:template name="mir.jumbotwo">
     <!-- show only on startpage -->
     <xsl:if test="//div/@class='jumbotwo'">
-      <div class="jumbotron">
-        <div class="container">
-          <h1>Mit MIR wird alles gut!</h1>
-          <h2>your repository - just out of the box</h2>
-        </div>
-      </div>
     </xsl:if>
   </xsl:template>
 
--- save/webpages/js/fhswf.js
+++ save/webpages/js/fhswf.js
@@ -9,10 +9,18 @@ $(document).ready(function() {
       $(this).remove();
   });
 
+  // activate empty search on start page
+  $("#project-searchMainPage").submit(function (evt) {
+    $(this).find(":input").filter(function () {
+          return !this.value;
+      }).attr("disabled", true);
+    return true;
+  });
+
   // add login link if access is denied
-  $("div.alert-warning:contains('nur angemeldete Nutzer')").replaceWith(function () { 
+  $("div.alert-warning:contains('nur angemeldete Nutzer')").replaceWith(function () {
       return $('<div role="alert" class="alert alert-warning"><strong>Zugriffsberechtigung</strong>&nbsp;<a href="https://publikationen.fhb.fh-swf.de/servlets/MCRLoginServlet">nur angemeldete Nutzer</a></div>');
   });
 
-  
+
 });
